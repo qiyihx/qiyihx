15e8a8002a5f288b3537906011c995c20a676d10
张庆 <zhang_qing@suixingpay.com>
2017-08-12 23:54:43 +0800

    保存活动

--- api/activity/save.php
+++ api/activity/save.php
@@ -13,7 +13,7 @@ require_once substr(dirname(__FILE__),0,-13).'/api/wxapp/lib/WxApp.Config.php';
 require_once substr(dirname(__FILE__),0,-13)."/api/wxapp//lib/WxApp.Api.php";
 
 
-$result = array('retcode' => 'SUCCESS', 'retmsg' => '活动发起成功');
+$result = array('retcode' => 'SUCCESS', 'retmsg' => '活动发起成功，请耐心等待审核');
 
 $wxAppApi = new WxAppApi();
 $openidObj = $wxAppApi->getOpenid(WxAppConfig::APPID, WxAppConfig::APPSECRET,$_GET['code']);
@@ -28,6 +28,7 @@ $info['picurl'] = $_GET['picurl'];
 $info['date'] = $_GET['date'];
 $info['time'] = $_GET['time'];
 $info['user_name'] = $_GET['user_name'];
+$info['user_headimg'] = $_GET['user_headimg'];
 $info['form_id'] = $_GET['form_id'];
 
 $result['data'] = Activity::save($info);
@@ -45,7 +46,7 @@ if($result['data'] == 0){
 	$data = array('keyword1' => $value1,'keyword2' => $value2,'keyword3' => $value3,'keyword4' => $value4,'keyword5' => $value5, 'keyword6' => $value6);
 	$result['resultSendTplMsg'] = $wxAppApi->sendTplMsg($info['openid'], WxAppConfig::WX_TPL_MSG_ACTIVITY_CREATE, $info['form_id'], $data, 'pages/activity-list/index?from=me', $timeOut = 6);
 }else if($result['data'] == 'true'){
-	$result['retmsg'] = "活动更新成功";
+	$result['retmsg'] = "活动更新成功，请耐心等待审核";
 }else{
 	$result['retcode'] = "FAIL";
 	$result['retmsg'] = "操作失败，请重试";
